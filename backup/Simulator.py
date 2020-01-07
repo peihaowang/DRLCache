e9c109eb7b4f77741eb0318280cca57f65018f5b
RIO <wangrui3@shanghaitech.edu.cn>
2020-01-07 23:02:25 +0800

    for wph to play

--- backup/Simulator.py
+++ backup/Simulator.py
@@ -3,9 +3,6 @@ import csv
 import pandas
 import numpy as np
 
-# file directory 
-fd = r'//home/rio/Desktop/AI/CS130-Pintos-src/filesys/build/tests/vm'
-
 # cacheline
 class cacheline:
 	def __init__(self,s,a):
@@ -61,6 +58,17 @@ class cache():
 		self.miss = self.miss + 1
 		return False
 
+	# find if the sector is cached for MRU
+	def find_sector_mru(self,sector):
+		for i in self.blocks:
+			if sector == i.getsector():
+				self.hit = self.hit + 1
+				self.blocks.append(i)
+				self.blocks.remove(i)
+				return True
+		self.miss = self.miss + 1
+		return False
+
 	# find if the sector is chached for clock
 	def find_sector_clock(self,sector):
 		for i in self.blocks:
@@ -101,6 +109,37 @@ class cache():
 		else:
 			del self.blocks[0]
 			self.blocks.append(cacheline(sector,0))
+			return
+
+	# MRU_al
+	def mru(self,sector):
+		# if in cache
+		if self.find_sector_mru(sector) == True:
+			return
+
+		# if not full
+		if self.number < self.size:
+			self.blocks.append(cacheline(sector,0))
+			self.number = self.number + 1
+			return
+		else:
+			self.blocks.pop()
+			self.blocks.append(cacheline(sector,0))
+			return
+
+	# radom_al
+	def rdm(self,sector):
+		# if in cache
+		if self.find_sector_fifo(sector) == True:
+			return 
+
+		# if not full, append to the end of the list
+		if self.number < self.size:
+			self.blocks.append(cacheline(sector,0))
+			self.number = self.number + 1
+			return 
+		else:
+			self.blocks[random.randint(0,self.size-1)] = cacheline(sector,0)
 			return
 
 	# clock_al
@@ -161,7 +200,7 @@ def eachfile(fd):
 	outputfile = []
 	path = os.listdir(fd)
 
-	headers = ["TESTNAME","FIFO32","FIFO64","LRU32","LRU64","CLOCK32","CLOCK64"]
+	headers = ["TESTNAME","FIFO","LRU","MRU","CLOCK","RANDOM"]
 	result = open("result.csv", 'w', newline = '')
 	result_csv = csv.writer(result)
 	result_csv.writerow(headers)
@@ -175,24 +214,16 @@ def eachfile(fd):
 			testname = os.path.splitext(os.path.split(newdir)[1])[0]
 			df = pandas.read_csv(newdir)
 
-			fifo_cache64 = cache(64)
-			lru_cache64 = cache(64)
-			clock_cache64 = cache(64)
-			fifo_cache32 = cache(32)
-			lru_cache32 = cache(32)
-			clock_cache32 = cache(32)
+			test1,test2,test3,test4,test5 = cache(64),cache(64),cache(64),cache(64),cache(64)
+
 			for i in df["blocksector"]:
-				fifo_cache64.fifo(i)
-				lru_cache64.lru(i)
-				clock_cache64.clk(i)
-				fifo_cache32.fifo(i)
-				lru_cache32.lru(i)
-				clock_cache32.clk(i)
-			result_csv.writerow([testname,fifo_cache32.get_missrate(),fifo_cache64.get_missrate(),lru_cache32.get_missrate(),lru_cache64.get_missrate(),clock_cache32.get_missrate(),clock_cache64.get_missrate()])
+				test1.fifo(i)
+				test2.lru(i)
+				test3.mru(i)
+				test4.clk(i)
+				test5.rdm(i)
+
+			result_csv.writerow([testname,test1.get_missrate(),test2.get_missrate(),test3.get_missrate(),test4.get_missrate(),test5.get_missrate()])
 			print(testname + " is finished")
 
-test = cache(64)
-zipf = np.random.zipf(2.,1000)
-for i in zipf:
-	test.clk(i)
-print(test.get_missrate())
+eachfile(fd)
